572595c8c7489fae17567af5d30de6b2b1f140ef
Hfn <55591161+titocs@users.noreply.github.com>
2021-11-08 21:23:11 +0700

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-echo "Masukkan nama: ";
-read a;
-echo "Selamat datang $a, masukkan nama belakangmu: ";
-read b;
-echo "Thanks $a $b for telling us your name";
+echo "Masukkan nama: "
+read a
+echo "Selamat datang $a, masukkan nama belakangmu: "
+read b
+echo "Thanks $a $b for telling us your name"
+
+# for same line
+read -p a
