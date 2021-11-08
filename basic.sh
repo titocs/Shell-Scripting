203808f00f4dee3c8f825bb0ec91f701c3bd4209
Hfn <55591161+titocs@users.noreply.github.com>
2021-11-08 21:47:56 +0700

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# ------------ Taking Input ----------------
 echo "Masukkan nama: "
 read a
 echo "Selamat datang $a, masukkan nama belakangmu: "
@@ -6,4 +7,15 @@ read b
 echo "Thanks $a $b for telling us your name"
 
 # for same line
-read -p "username: " a
+read -p "username: " username
+# for silence input
+read -sp "password: " password
+# input like an array
+read -a var
+echo ${var[0]} # and so on
+
+# ------------ Passing args ----------------
+echo $0 $1 $2 $3
+args = ("$@")
+echo $@ # will be print all index
+echo $# # Counting element
