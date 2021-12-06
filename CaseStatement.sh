8fd6afff698bd51ad741f57bd630d9af812da3bd
Hfn <55591161+titocs@users.noreply.github.com>
2021-12-06 21:45:48 +0700

    Create CaseStatement.sh

---
+++ CaseStatement.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+vehicle = $1 # to accept argument
+
+case $vehicle in
+    "car" )
+        echo "Rent of $vehicle is 100 dollar" ;;
+    "van" )
+        echo "Rent of $vehicle is 80 dollar" ;;
+    "bicycle" )
+        echo "Rent of $vehicle is 8 dollar" ;;
+    * )
+        echo "Uknown vehicle" ;;
+esac
