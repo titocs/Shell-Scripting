ab3826b0020276b0f63d5c9dd7b1c277bb0388ce
Hfn <55591161+titocs@users.noreply.github.com>
2021-12-06 17:18:22 +0700

    Create LogicalOR.sh

---
+++ LogicalOR.sh
@@ -0,0 +1,10 @@
+#! bin/bash
+
+# for using OR operator use ||
+if [ "$age" -gt 18] || ["$age" -lt 30 ]
+
+# The -o option provide an alternative compound condition test.
+if [ "$age" -gt 18 -o "$age" -lt 30 ]
+
+# if [[ $condition1 || $condition2 ]]    # Also works.
+if [[ "$age" -gt 18 || "$age" -lt 30 ]]
