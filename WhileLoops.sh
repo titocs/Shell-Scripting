fe2df24f0964766a0b9d36d387943b322c40685b
Hfn <55591161+titocs@users.noreply.github.com>
2021-12-14 16:58:02 +0700

    Create WhileLoops.sh

---
+++ WhileLoops.sh
@@ -0,0 +1,22 @@
+#! bin/bash
+
+n = 1
+while [ $n -le 10 ] # or (($n <= 10))
+do
+  echo "$n"
+  n = $((n + 1)) # can be change by : (( ++n ))
+done
+
+# output: 
+' ---------------
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'
